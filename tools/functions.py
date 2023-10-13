450e50c4b5656f1c0abe1c8c6e1d8d1b908b8a2f
vism <vismiroglou@gmail.com>
2023-10-13 08:36:01 +0200

    Initial commit

---
+++ tools/functions.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def create_complete_datasets(data_dict):
+    '''
+    Function for creating complete training and test sets containing
+    all classes.
+    '''
+    #Empty list
+    trainset = []
+    traintargets =[]
+    testset = []
+    testtargets =[]
+    
+    #For each class
+    for i in range(10):
+        trainset.append(data_dict["train%d"%i])
+        traintargets.append(np.full(len(data_dict["train%d"%i]),i))
+        testset.append(data_dict["test%d"%i])
+        testtargets.append(np.full(len(data_dict["test%d"%i]),i))
+    
+    #Concatenate into to complete datasets
+    trainset = np.concatenate(trainset)
+    traintargets = np.concatenate(traintargets)
+    testset = np.concatenate(testset)
+    testtargets = np.concatenate(testtargets)
+    return trainset, traintargets, testset, testtargets
